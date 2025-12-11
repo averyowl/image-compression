cf9ac6f4172884e93b2b697ffaaeaca4eb992919
jamesxtruong <jamesxtruong@gmail.com>
2025-12-10 23:02:57 -0800

    Add task comments. Set k10, 30, 50 for task 14.

--- lab14.m
+++ lab14.m
@@ -1,3 +1,6 @@
+%% Lab 14: Singular Value Decomposition and Image Compression
+
+%% Task 1: 
 %% 2x2 matrix
 
 clear;
@@ -12,11 +15,13 @@ axis equal
 title('Unit circle')
 hold off;
 
+%% Task 2: 
 A = [ 2, 1; -1, 1 ];
 [U,S,V] = svd(A);
 U'*U
 V'*V
 
+%% Task 3: 
 VX=V'*X;
 subplot(2,2,2)
 hold on;
@@ -27,7 +32,7 @@ axis equal
 title('Multiplied by matrix V^T')
 hold off;
 
-
+%% Task 4:
 SVX = S*VX;
 subplot(2,2,3);
 hold on;
@@ -38,6 +43,7 @@ axis equal
 title('Multiplied by matrix \Sigma V^T')
 hold off;
 
+%% Task 5:
 AX = U*SVX;
 subplot(2,2,4)
 hold on;
@@ -50,7 +56,7 @@ axis equal
 title('Multiplied by matrix U\Sigma V^T=A')
 hold off;
 
-
+%% Task 6:
 %% Modified SVD
 U1(:,1)=U(:,1);
 U1(:,2)=-U(:,2);
@@ -59,10 +65,11 @@ V1(:,2)=-V(:,2);
 
 U1*S*V1'-A
 
+%% Task 7:
 %% Check
 A*V-U*S
 
-
+%% Task 8:
 %% Image compression
 
 % Creates a two-dimensional array with the dimensions equal to the dimensions of 
@@ -74,15 +81,16 @@ imshow(ImJPG);
 
 [m,n]=size(ImJPG);
 
+%% Task 9: 
 % Compute an SVD
-
 [UIm,SIm,VIm]=svd(double(ImJPG));
 
+%% Task 10:
 % plot the singular values
 figure;
 plot(1:min(m,n),diag(SIm));
 
-
+%% Task 11:
 %% Create approximations to the image
 
 % With 50, 100, and 150 singular values
@@ -96,7 +104,7 @@ for k=50:50:150
 end;
 
 
-
+%% Task 12:
 %% Noise filtering
 
 clear;
@@ -111,13 +119,15 @@ imshow(ImJPG);
 figure;
 imshow(uint8(ImJPG_Noisy));
 
-
+%% Task 13:
 [UIm,SIm,VIm]=svd(ImJPG_Noisy);
 
 figure;
 plot(1:min(m,n),diag(SIm),'ko');
 
-for k=10:20:50
+%% Task 14:
+% for k=10:20:50
+for k=10:30:50 
     ImJPG_comp=uint8(UIm(:,1:k)*SIm(1:k,1:k)*(VIm(:,1:k))');
     figure;
     imshow(ImJPG_comp)
